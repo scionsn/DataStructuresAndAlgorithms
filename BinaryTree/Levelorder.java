5595bb7ba46f311301cd87a9e82230ae25f5bc42
scionsn <52966844+scionsn@users.noreply.github.com>
2021-10-05 21:05:00 +0530

    Update Levelorder.java

--- BinaryTree/Levelorder.java
+++ BinaryTree/Levelorder.java
@@ -1,4 +1,26 @@
 
 public class Levelorder {
+public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return null;
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.addLast(root);
+        while (!q.isEmpty()) {
+            List<Integer> sub = new ArrayList<Integer>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.peek();
+                if (node.left != null)
+                    q.addLast(node.left);
+                if (node.right != null)
+                    q.addLast(node.right);
+                int poll = q.removeFirst().val;
+                System.out.println(poll);
+                sub.add(poll);
+            }
+            res.add(sub);
 
+        }
+        return res;
 }
